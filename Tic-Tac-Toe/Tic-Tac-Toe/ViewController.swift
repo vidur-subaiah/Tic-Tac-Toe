275524f05d51bae1986b9ad4c03cb68c3171b777
vidur-subaiah <47302924+vidur-subaiah@users.noreply.github.com>
2023-01-29 15:30:02 -0600

    App Skeleton Ready

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Vidur Subaiah on 1/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
